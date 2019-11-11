1f349798f5737c55d2122ae2c043da15dce3f549
Whittney Davis <wcdavis89@yahoo.com>
2019-11-11 21:08:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,9 @@ def take_a_number(array,name)
   ticket=0 
   array.each << name do
     ticket+=1
+    p "Welcome, #{name}. You are number #{ticket} in line."
   end
 end
 
 def now_serving(name,num)
-  
+end
