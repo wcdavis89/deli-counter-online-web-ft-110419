8c3a1994e1672cc8792c40450819bb55c4836179
Whittney Davis <wcdavis89@yahoo.com>
2019-11-11 21:07:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(array)
+  if array.size=0 
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array,name)
+  ticket=0 
+  array.each << name do
+    ticket+=1
+  end
+end
+
+def now_serving(name,num)
+  
