754c804254c3eef2037760ad6708daba8d61a8e4
Whittney Davis <wcdavis89@yahoo.com>
2019-11-11 21:32:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array.size<1
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. #{array[0].join(", ")}   "
+    puts "The line is currently: 1. #{array.join(", ")}   "
   end
 end
 
