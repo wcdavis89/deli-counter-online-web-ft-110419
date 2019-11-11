54e38cb00a6bddc90d0ab1a2e91c4e13ae55042a
Whittney Davis <wcdavis89@yahoo.com>
2019-11-11 22:06:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
     puts "The line is currently empty."
   else
     array.each_with_index do |name,index|  end
-    puts "The line is currently: #{index}. #{array.join(" #{index}. ")}"
+    puts "The line is currently: #{index}. #{name}"
   end
 end
 
